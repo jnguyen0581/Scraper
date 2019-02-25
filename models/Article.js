d58be4a7bc549db2ef97b466e5c0f10e5de0483c
JoyceNguyen <jnguyen0581@gmail.com>
2019-02-25 11:58:08 -0600

    Scraping working

--- models/Article.js
+++ models/Article.js
@@ -1,35 +1,32 @@
-// Require mongoose
 var mongoose = require("mongoose");
-var Note = require("./Note");
-// Create Schema class
+
+// Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Create article schema
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
 var ArticleSchema = new Schema({
+  // `title` is required and of type String
   title: {
     type: String,
     required: true
   },
-  summary: {
-    type: String,
-    required: true
-  },
+  // `link` is required and of type String
   link: {
     type: String,
     required: true
   },
-  saved: {
-    type: Boolean,
-    default: false
-  },
-  notes: [{
-     type: Schema.Types.ObjectId,
-     ref: "Note"
-  }]
+  // `note` is an object that stores a Note id
+  // The ref property links the ObjectId to the Note model
+  // This allows us to populate the Article with an associated Note
+  note: {
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }
 });
 
-// Create the Article model with the ArticleSchema
+// This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
 
-// Export the model
+// Export the Article model
 module.exports = Article;
