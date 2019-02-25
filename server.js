73379468ce569356471744d8ab6c13b742960d42
JoyceNguyen <jnguyen0581@gmail.com>
2019-02-25 12:02:37 -0600

    update db URI

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/scrapedb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapedb";
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 // Routes
 
